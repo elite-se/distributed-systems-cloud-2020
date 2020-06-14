a2a72b082ef01a8a4bcd84be145ec500dbd70853
Roman Buchholz <roman.buchholz@t-online.de>
2020-06-14 17:46:14 +0200

    fixed install script and brackets

--- helm/observability/prometheus/install.sh
+++ helm/observability/prometheus/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if helm list | grep -q prometheus; then
-    helm upgrade prometheus stable/prometheus --values values.yml
+    helm upgrade prometheus stable/prometheus -f values.yml
 else
     helm install prometheus stable/prometheus -f values.yml
 fi
