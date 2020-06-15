5d257bb940a297760dc7dfcc2ef604149491c822
Roman Buchholz <roman.buchholz@t-online.de>
2020-06-15 15:17:55 +0200

    added 2nd node

--- resources.tf
+++ resources.tf
@@ -45,7 +45,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "dis-sys-pool-1"
   location   = var.zone
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 2
 
   node_config {
     preemptible  = false
