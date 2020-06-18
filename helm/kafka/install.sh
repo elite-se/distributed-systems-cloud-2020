1b9d31fcc3679018f89ae73547c14d9a947ea3ab
Roman Buchholz <roman.buchholz@t-online.de>
2020-06-18 16:04:13 +0200

    fixed install and reduced zookeaper replicas

--- helm/kafka/install.sh
+++ helm/kafka/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if helm list | grep -q kafka; then
-    helm upgrade kafka incubator/kafka -f values.yml
+    helm upgrade kafka incubator/kafka -f values.yaml
 else
-    helm install kafka incubator/kafka -f values.yml
+    helm install kafka incubator/kafka -f values.yaml
 fi
