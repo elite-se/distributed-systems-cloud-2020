ac693c0a788d07dacdaf8515c9211f52c35bf017
Dominik Horn <dominik.horn@tum.de>
2020-06-20 14:09:22 +0200

    add mongodb kotlin client dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,9 +7,17 @@ dependencies {
     implementation("org.apache.kafka:kafka-clients:2.5.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.9.6")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.6")
+    implementation("org.mongodb:mongodb-driver-sync:4.0.4")
+    implementation("org.litote.kmongo:kmongo:4.0.2")
     implementation("org.apache.kafka:kafka-streams:2.0.0")
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
