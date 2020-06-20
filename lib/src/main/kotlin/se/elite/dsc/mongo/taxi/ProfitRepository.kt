5568c379e55276057e38b86e1bdcf2a91437c998
Dominik Horn <dominik.horn@tum.de>
2020-06-20 16:54:38 +0200

    implement logic for adding data from a single trip to profits

--- lib/src/main/kotlin/se/elite/dsc/mongo/taxi/ProfitRepository.kt
+++ lib/src/main/kotlin/se/elite/dsc/mongo/taxi/ProfitRepository.kt
@@ -1,6 +1,7 @@
 package se.elite.dsc.mongo.taxi
 
 import org.litote.kmongo.*
+import se.elite.dsc.kafka.taxi.Cell
 
 object ProfitRepository {
     private val database = MongoDB.client.getDatabase("profits")
@@ -8,5 +9,20 @@ object ProfitRepository {
 
     fun initialize() {
         collection.createIndex("{ \"dayOfYear\": 1, \"cell\": 1 }")
+    }
+
+    fun addTrip(tripDayOfYear: Int, cell: Cell, fareAmount: Double, tipAmount: Double) {
+        MongoDB.withTransaction { session ->
+            val profitData = collection.findOne(
+                    session,
+                    and(Profit::dayOfYear eq tripDayOfYear, Profit::cell eq cell)
+            ) ?: Profit(tripDayOfYear, cell, 0, 0.0, 0.0)
+
+            profitData.tripCount += 1
+            profitData.fareSum += fareAmount
+            profitData.tipSum += tipAmount
+
+            collection.save(profitData)
+        }
     }
 }
