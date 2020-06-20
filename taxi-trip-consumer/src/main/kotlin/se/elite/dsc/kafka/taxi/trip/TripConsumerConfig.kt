061ff94114326529cb3703bcf1d262d2bf3bbda4
Dominik Horn <dominik.horn@tum.de>
2020-06-20 16:56:23 +0200

    implement writing cell profit data to mongodb in TripConsumer

--- taxi-trip-consumer/src/main/kotlin/se/elite/dsc/kafka/taxi/trip/TripConsumerConfig.kt
+++ taxi-trip-consumer/src/main/kotlin/se/elite/dsc/kafka/taxi/trip/TripConsumerConfig.kt
@@ -1,8 +1,10 @@
 package se.elite.dsc.kafka.taxi.trip
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.StringDeserializer
+import se.elite.dsc.kafka.JsonObjectSerde
 import se.elite.dsc.kafka.KafkaConfig
+import se.elite.dsc.kafka.taxi.Cell
+import se.elite.dsc.kafka.taxi.Trip
 import java.util.*
 
 class TripConsumerConfig(val groupId: String, val sourcetopic: String) : KafkaConfig() {
@@ -12,8 +14,8 @@ class TripConsumerConfig(val groupId: String, val sourcetopic: String) : KafkaCo
         props[ConsumerConfig.GROUP_ID_CONFIG] = groupId
         props[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "false"
         props[ConsumerConfig.ISOLATION_LEVEL_CONFIG] = "read_committed"
-        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java;
-        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java;
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = JsonObjectSerde(Cell::class.java)
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonObjectSerde(Trip::class.java)
 
         return props
     }
