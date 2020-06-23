b66cc78f90fa028b26022b26b4179e7a8e2d4644
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-06-23 09:12:22 +0200

    use full taxi data

--- taxi-producer/src/main/kotlin/se/elite/dsc/kafka/taxi/TaxiProducerMain.kt
+++ taxi-producer/src/main/kotlin/se/elite/dsc/kafka/taxi/TaxiProducerMain.kt
@@ -4,6 +4,6 @@ import com.google.common.io.Resources
 
 fun main(args: Array<String>) {
     val config = TaxiProducerConfig("taxi-source")
-    val datafile = Resources.getResource("taxi_data_2019_01.csv").path
-    TaxiProducer(config, datafile).produce(100)
+    val datafile = Resources.getResource("taxi_data.csv").path
+    TaxiProducer(config, datafile).produce(1)
 }
