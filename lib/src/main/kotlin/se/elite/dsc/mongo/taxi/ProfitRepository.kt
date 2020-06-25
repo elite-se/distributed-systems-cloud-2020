7724530b96430b46418de18f602a2b8451b5a4ed
Dominik Horn <dominik.horn@tum.de>
2020-06-25 11:05:53 +0200

    prevent duplicate entries in MongoDB
    Sadly, transactional semantics are only available in MongoDB Replica clusters
    (Hope terminology is correct, Idk). I am not eager to deploy such a thing to
    cloud as it currently requires some manual local scripting. Therefore, transactions
    are disabled in this patch

--- lib/src/main/kotlin/se/elite/dsc/mongo/taxi/ProfitRepository.kt
+++ lib/src/main/kotlin/se/elite/dsc/mongo/taxi/ProfitRepository.kt
@@ -1,5 +1,6 @@
 package se.elite.dsc.mongo.taxi
 
+import com.mongodb.client.model.UpdateOptions
 import org.litote.kmongo.*
 import se.elite.dsc.kafka.taxi.Cell
 
@@ -12,17 +13,27 @@ object ProfitRepository {
     }
 
     fun addTrip(tripDayOfYear: Int, cell: Cell, fareAmount: Double, tipAmount: Double) {
-        MongoDB.withTransaction { session ->
-            val profitData = collection.findOne(
-                    session,
-                    and(Profit::dayOfYear eq tripDayOfYear, Profit::cell eq cell)
-            ) ?: Profit(tripDayOfYear, cell, 0, 0.0, 0.0)
+        // Transactions requires deploying a special mongo cluster which I'm not eager to do
+        // beyond localhost
+//        MongoDB.withTransaction { session ->
+        val filter = and(Profit::dayOfYear eq tripDayOfYear, Profit::cell eq cell)
+        val mongoResult = collection.findOne(
+//                    session,
+                filter
+        )
+        val profitData = mongoResult ?: Profit(tripDayOfYear, cell, 0, 0.0, 0.0)
 
-            profitData.tripCount += 1
-            profitData.fareSum += fareAmount
-            profitData.tipSum += tipAmount
+        profitData.tripCount += 1
+        profitData.fareSum += fareAmount
+        profitData.tipSum += tipAmount
 
-            collection.save(profitData)
+        if (mongoResult != null) {
+            println("Cell ${profitData.cell} already has a value on day ${profitData.dayOfYear} => Updating")
+            collection.updateOne(filter, profitData, UpdateOptions())
+        } else {
+            println("Inserting new value for cell ${profitData.cell} and day ${profitData.dayOfYear}")
+            collection.insertOne(profitData)
         }
+//        }
     }
 }
