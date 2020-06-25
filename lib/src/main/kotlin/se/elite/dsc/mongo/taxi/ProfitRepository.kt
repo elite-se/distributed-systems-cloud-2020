d5ea6096186935fd274fec0b2b2a90208c4efb7f
Dominik Horn <dominik.horn@tum.de>
2020-06-25 11:17:43 +0200

    simplify ProfitRepository addTrip() code

--- lib/src/main/kotlin/se/elite/dsc/mongo/taxi/ProfitRepository.kt
+++ lib/src/main/kotlin/se/elite/dsc/mongo/taxi/ProfitRepository.kt
@@ -13,27 +13,14 @@ object ProfitRepository {
     }
 
     fun addTrip(tripDayOfYear: Int, cell: Cell, fareAmount: Double, tipAmount: Double) {
-        // Transactions requires deploying a special mongo cluster which I'm not eager to do
-        // beyond localhost
-//        MongoDB.withTransaction { session ->
         val filter = and(Profit::dayOfYear eq tripDayOfYear, Profit::cell eq cell)
-        val mongoResult = collection.findOne(
-//                    session,
-                filter
-        )
+        val mongoResult = collection.findOne(filter)
         val profitData = mongoResult ?: Profit(tripDayOfYear, cell, 0, 0.0, 0.0)
 
         profitData.tripCount += 1
         profitData.fareSum += fareAmount
         profitData.tipSum += tipAmount
 
-        if (mongoResult != null) {
-            println("Cell ${profitData.cell} already has a value on day ${profitData.dayOfYear} => Updating")
-            collection.updateOne(filter, profitData, UpdateOptions())
-        } else {
-            println("Inserting new value for cell ${profitData.cell} and day ${profitData.dayOfYear}")
-            collection.insertOne(profitData)
-        }
-//        }
+        collection.updateOne(filter, profitData, UpdateOptions().upsert(true))
     }
 }
